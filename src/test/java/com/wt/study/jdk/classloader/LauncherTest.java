57f2d6063ea94b0e570457486ebd69e7c170e104
yn <280573787@qq.com>
2019-01-11 17:02:45 +0800

    æµ添加Loader测试

--- src/test/java/com/wt/study/jdk/classloader/LauncherTest.java
+++ src/test/java/com/wt/study/jdk/classloader/LauncherTest.java
@@ -2,12 +2,15 @@ package com.wt.study.jdk.classloader;
 
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class LauncherTest {
     /**
      * BootStrapLoader加载类的路径
      */
     @Test
-    public void test(){
+    public void test() {
         Launcher launcher = new Launcher();
         launcher.getbootClassPath();
     }
@@ -16,7 +19,7 @@ public class LauncherTest {
      * ExtClassLoader加载类的路径
      */
     @Test
-    public void test1(){
+    public void test1() {
         Launcher launcher = new Launcher();
         launcher.getExtClassLoader();
     }
@@ -25,21 +28,56 @@ public class LauncherTest {
      * AppClassLoader加载类的路径
      */
     @Test
-    public void test2(){
+    public void test2() {
         Launcher launcher = new Launcher();
         launcher.getClassPath();
     }
 
+    /**
+     * 测试ClassLoader的父类
+     */
     @Test
-    public void test3(){
+    public void test3() {
         ClassLoader classLoader = Launcher.class.getClassLoader();
-        System.out.println("自定义类ClassLoader is"+classLoader.toString());
+        System.out.println("自定义类ClassLoader is" + classLoader.toString());
         System.out.println("APPClassLoader父类 is" + classLoader.getParent().toString());
         System.out.println("ExtClassLoader父类 is" + classLoader.getParent().getParent().toString());
 
 
         ClassLoader classLoader1 = String.class.getClassLoader();
-        System.out.println("ClassLoader is"+classLoader1.toString());
+        System.out.println("ClassLoader is" + classLoader1.toString());
+    }
+
+    /**
+     * 测试自定义Loader
+     */
+    @Test
+    public void test4() {
+//创建自定义classloader对象。
+        DiskClassLoader diskLoader = new DiskClassLoader("D:\\download\\");
+        try {
+            //加载class文件
+            Class c = diskLoader.loadClass("com.wt.study.jdk.classloader.CustomerLoaderTarget");
+
+            if (c != null) {
+                try {
+                    Object obj = c.newInstance();
+                    Method method = c.getDeclaredMethod("say", null);
+                    //通过反射调用Test类的say方法
+                    method.invoke(obj, null);
+                } catch (InstantiationException | IllegalAccessException
+                        | NoSuchMethodException
+                        | SecurityException |
+                        IllegalArgumentException |
+                        InvocationTargetException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
